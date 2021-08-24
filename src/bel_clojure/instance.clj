d9facaff06ef479865e942552b39c4e510f7454c
stopachka <stepan.p@gmail.com>
2021-08-23 20:26:21 -0400

    sm

--- src/bel_clojure/instance.clj
+++ src/bel_clojure/instance.clj
@@ -25,8 +25,43 @@
 (def bel-scope [:symbol "scope"])
 (def bel-if [:symbol "if"])
 
+;; Pair
+;; ----
+
+(defn make-pair [a b]
+  (ArrayList. [:pair a b]))
+
+(defn make-quoted-pair [a]
+  (make-pair bel-quote a))
+
+(declare p-car)
+(declare p-cdr)
+
+(defn pair->clojure-seq [[_t l [r-t :as r] :as form]]
+  (if (= bel-nil form)
+    ()
+    (cons
+     l
+     (cond
+       (= :pair r-t) (pair->clojure-seq r)
+       (= bel-nil r) []
+       :else [r]))))
+
+(defn pair-find [f p]
+  (cond
+    (= bel-nil p) p
+    (f (p-car p)) (p-car p)
+    :else (pair-find f (p-cdr p))))
+
+(defn pair-map [f p]
+  (if (= bel-nil p) bel-nil
+      (make-pair
+       (f (p-car p))
+       (pair-map f (p-cdr p)))))
+
 ;; Reader
 ;; ------
+
 
 (defn form-transform
   [k f]
@@ -36,12 +71,6 @@
          (= (first x) k))
       (f x)
       x)))
-
-(defn make-pair [a b]
-  (ArrayList. [:pair a b]))
-
-(defn make-quoted-pair [a]
-  (make-pair bel-quote a))
 
 (defn first-and-only [xs msg]
   (assert (= (count xs) 1) msg)
@@ -176,14 +205,6 @@
 (comment
   (p-xdr (bel-parse "(a . b)") (bel-parse "c")))
 
-(defn pair->clojure-seq [[_t l [r-t :as r]]]
-  (cons
-   l
-   (cond
-     (= :pair r-t) (pair->clojure-seq r)
-     (= bel-nil r) []
-     :else [r])))
-
 (defn p-sym [char-pairs]
   (let [cs (pair->clojure-seq char-pairs)
         _  (assert (every? (comp (partial = :char) first) cs)
@@ -236,13 +257,10 @@
 
 (declare bel-eval)
 
-(defn find-val [sym-to-find pairs]
-  (some->> pairs
-           pair->clojure-seq
-           (filter (fn [[_ sym]]
-                     (= sym-to-find sym)))
-           first
-           p-cdr))
+(defn find-env-pair [sym-to-find pairs]
+  (let [v (some->> pairs
+                   (pair-find (fn [p] (= sym-to-find (p-car p)))))]
+    (when (not= v bel-nil) v)))
 
 (defn eval-symbol [{:keys [scope globe]} form]
   (cond
@@ -251,27 +269,18 @@
     (= bel-scope form) scope
     :else
     (let [v (->> [scope globe]
-                 (some (partial find-val form)))]
+                 (some (partial find-env-pair form)))]
       (assert v (format "expected value for symbol = %s" form))
-
-      v)))
+      (p-cdr v))))
 
 (comment
-  (eval-symbol {:globe (make-bel-globe)} [:symbol "id"]))
+  (eval-symbol {:scope bel-nil
+                :globe (make-bel-globe)} [:symbol "id"]))
 
-(defn map-pair [f p]
-  (if (= bel-nil p) bel-nil
-      (make-pair
-       (f (p-car p))
-       (map-pair f (p-cdr p)))))
-
-(defn args-head->evaled-args-seq [env args-head]
-  (map (partial bel-eval env) (pair->clojure-seq args-head)))
-
-(defn exec-prim [env r args-head]
+(defn eval-prim [_env r args-head]
   (let [[_ [_ n]] r
-        args (args-head->evaled-args-seq env args-head)
         f (prim-name->fn n)
+        args (pair->clojure-seq args-head)
         niled-args (->> f
                         meta
                         :arglists
@@ -283,39 +292,58 @@
 
     (apply f niled-args)))
 
-(defn make-var-pair [[sym-t :as sym] evaled]
+(defn make-env-pair [[sym-t :as sym] v]
   (assert (= :symbol sym-t)
-          (format
-           "expected left-side of set to be a symbol = %s" sym))
-  (make-pair sym evaled))
+          (format "expected left-side of to be a symbol = %s" sym))
+  (make-pair sym v))
 
-(defn exec-set [{:keys [globe] :as env} [_ _ r]]
-  (->> r
-       pair->clojure-seq
-       (partition 2)
-       (map (fn [[sym v]]
-              (let [to-set (bel-eval env v)
-                    [_ head tail] globe]
-                (p-xar globe (make-var-pair sym to-set))
-                (p-xdr globe (make-pair head tail)))))
-       doall))
+(defn eval-set [{:keys [globe] :as env} [_ _ r]]
+  (letfn [(f [p]
+            (let [[_ sym after-sym] p
+                  _ (assert (not= bel-nil after-sym)
+                            "Set sym needs a value")
+                  [_ v after-v] after-sym
+                  evaled-v (bel-eval env v)
+                  [_ head tail] globe]
+              (p-xar globe (make-env-pair sym evaled-v))
+              (p-xdr globe (make-pair head tail))
+              (when (not= bel-nil after-v)
+                (f after-v))))]
+    (f r)))
 
-(defn exec-clo [env r args-seq]
-  (let [[scope args-var-head body-head] (pair->clojure-seq r)
-        args-vars (pair->clojure-seq args-var-head)
-        new-scope
-        (reduce (fn [s p] (make-pair p s))
-                scope
-                (map make-var-pair args-vars args-seq))]
-    (bel-eval
-     (assoc env :scope new-scope)
-     body-head)))
+(defn add-to-scope [scope [sym-t :as sym-head] arg-head]
+  (let [num-nil (->> [sym-head arg-head]
+                     (filter (partial = bel-nil))
+                     count)]
+    (cond
+      (= num-nil 2)
+      scope
 
-(defn exec-mac [env r args-head]
-  (let [code (exec-clo
-              env
-              (p-cdr (p-cdr (p-car r)))
-              (pair->clojure-seq args-head))]
+      (= num-nil 1)
+      (throw (Exception. "Symbols and argument lengths don't match!"))
+
+      (= :symbol sym-t)
+      (make-pair
+       (make-env-pair sym-head arg-head)
+       scope)
+
+      :else
+      (add-to-scope
+       (make-pair
+        (make-env-pair (p-car sym-head)
+                       (p-car arg-head))
+        scope)
+       (p-cdr sym-head)
+       (p-cdr arg-head)))))
+
+
+(defn eval-clo [env r args-head]
+  (let [[_ scope [_ args-sym-head [_ body-head]]] r
+        new-scope (add-to-scope scope args-sym-head args-head)]
+    (bel-eval (assoc env :scope new-scope) body-head)))
+
+(defn eval-mac [env [_ [_ _ [_ _ clo]]] args-head]
+  (let [code (eval-clo env clo args-head)]
     (bel-eval env code)))
 
 (defn eval-if [env [_ test-form [_ consequent-form r]]]
@@ -327,11 +355,14 @@
       (= bel-nil (p-cdr r)) (bel-eval env (p-car r))
       :else (eval-if env r))))
 
+(defn eval-pairs [env head]
+  (pair-map (partial bel-eval env) head))
+
 (defn eval-pair [env [_ l r :as x]]
   (cond
     (= bel-quote l) r
     (= bel-lit l) x
-    (= bel-set l) (exec-set env x)
+    (= bel-set l) (eval-set env x)
     (= bel-if l) (eval-if env r)
     :else
     (let [[_ f args-head] x
@@ -341,11 +372,11 @@
                     "error: expected lit expression as fn call")]
       (condp = t
         bel-prim
-        (exec-prim env r args-head)
+        (eval-prim env r (eval-pairs env args-head))
         bel-clo
-        (exec-clo env r (args-head->evaled-args-seq env args-head))
+        (eval-clo env r (eval-pairs env args-head))
         bel-mac
-        (exec-mac env r args-head)
+        (eval-mac env r args-head)
         (throw (Exception. "todo: unsupported fn call"))))))
 
 (defn bel-eval [env [t :as form]]
@@ -374,7 +405,7 @@
   (run "(id t t)")
   (run "(id t)")
   (run "(id)")
-  (run "(set a 'b)" "a")
+  (run "(set a 'b c 'd)" "c")
   (run "((lit clo nil (x) (id x t)) t)")
   (run "((lit clo nil (x) (id x t)) nil)")
   (run "globe")
@@ -405,7 +436,6 @@
 
 (comment
   (apply run (readable-source)))
-
 
 ; next up, let's stop using pair->clojure-seq -- just create pair helper fns
 ; then, do the smarter assignment for sym -> val on lambda
