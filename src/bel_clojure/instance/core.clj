244d8a1f63ce75551818aa5cb904e98fdacea94a
Stepan Parunashvili <stepan.p@gmail.com>
2021-12-26 13:21:59 -0500

    add tests (#5)
    * sm
    
    * sm
    
    * sm

---
+++ src/bel_clojure/instance/core.clj
@@ -0,0 +1,28 @@
+(ns bel-clojure.instance.core
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as cstring]
+   [bel-clojure.instance.evaluator :as e]))
+
+;; ------
+;; Bootstrap
+
+(defn readable-source []
+  (->> (-> (io/resource "source.bel")
+           slurp
+           (cstring/split #"\n"))
+       (partition-by cstring/blank?)
+       (map (fn [xs] (cstring/join "\n" xs)))
+       (remove (fn [s]
+                 (cstring/starts-with? s ";")))
+
+       (remove cstring/blank?)
+       (take-while
+        (fn [s] (not= s "===BREAK===")))))
+
+(defn bootstrap-env []
+  (let [env (e/make-env)]
+    (println "loading readable source...")
+    (e/eval-all env (readable-source))
+    env))
+
